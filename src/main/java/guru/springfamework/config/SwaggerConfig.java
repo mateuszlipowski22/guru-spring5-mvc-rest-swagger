9ecd16bde09a23087a86b1f31869d2b3fe2dbf39
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-06 23:48:08 +0100

    add metaData of API

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 /**
  * Created by jt on 12/20/17.
@@ -22,7 +26,23 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("Mateusz Lipowski", "Http://example.com.pl", "mojmail.@wp.pl");
+
+        return new ApiInfo(
+                "Spring 5 MVC REST API",
+                "Spring 5 ",
+                "1.0",
+                "Terms of service: nvm",
+                contact,
+                "License",
+                "http://license.com.pl",
+                new ArrayList<>());
     }
 
 }
